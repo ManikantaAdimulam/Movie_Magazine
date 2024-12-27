1bae655c4f4798de6ca1211339cbf3de0d53d058
Manikanta <adimulam.manikanta@nagarro.com>
2024-12-27 14:43:43 +0530

    clean up

--- src/screens/movies/movies.tsx
+++ src/screens/movies/movies.tsx
@@ -14,10 +14,12 @@ import Colors from '@utils/theme/colors';
 import Language from '@components/language';
 import MZText from '@components/text';
 
+const MOVIES_END_POINT =
+  'movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+
 const Movies = () => {
   const {isLoading, data, error} = useGetMoviesQuery({
-    endPoint:
-      'movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+    endPoint: MOVIES_END_POINT,
   });
 
   const renderItem = ({item: {poster_path, title}}: {item: Movie}) => {
